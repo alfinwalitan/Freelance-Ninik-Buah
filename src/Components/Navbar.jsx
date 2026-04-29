b134c57fe8e4a911e6eece295792f8797b4a71ab
alfinwalitan <alfin6010@gmail.com>
2026-04-29 18:02:58 +0700

    Responsive for Smaller Screen

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,11 +9,13 @@ function Navbar() {
     const [language, setLanguage] = useState('ID');
     const [isLangOpen, setIsLangOpen] = useState(false);
 
-    // Function for Language
+    // State for Hamburger
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // Function Change Language
     const handleLanguageChange = (lang) => {
         setLanguage(lang);
         setIsLangOpen(false);
-        // Will be Working Soon
     };
 
     return (
@@ -22,38 +24,53 @@ function Navbar() {
                 <h1>Ninik Buah</h1>
             </div>
 
-            <div className='navbar-menus'>
-                <ul>
-                    <li><a href='/'>Beranda</a></li>
-                    <li><a href='/'>Produk</a></li>
-                    <li><a href='/'>Blog</a></li>
-                    <li><a href='/'>Kontak</a></li>
-                </ul>
-            </div>
+            <button
+                className={`hamburger ${isMenuOpen ? 'open' : ''}`}
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                aria-label='Toggle menu'
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
 
-            <div className='navbar-menus'>
-                <ul>
-                    <li className='lang-menu-item'>
-                        <div
-                            className='lang-wrapper'
-                            onClick={() => setIsLangOpen(!isLangOpen)}
-                        >
-                            <i className='fa-solid fa-globe'></i>
-                            <span className='lang-selected-text'>{language}</span>
-                            <i className={`fa-solid fa-chevron-down navbar-chevron ${isLangOpen ? 'rotate' : ''}`}></i>
+            {isMenuOpen && (
+                <div className='nav-overlay' onClick={() => setIsMenuOpen(false)} />
+            )}
 
-                            {isLangOpen && (
-                                <ul className='lang-list'>
-                                    <li onClick={() => handleLanguageChange('ID')}>ID</li>
-                                    <li onClick={() => handleLanguageChange('EN')}>EN</li>
-                                </ul>
-                            )}
-                        </div>
-                    </li>
-                </ul>
+            <div className={`navbar-content ${isMenuOpen ? 'open' : ''}`}>
+                <div className='navbar-menus'>
+                    <ul>
+                        <li><a href='/' onClick={() => setIsMenuOpen(false)}>Beranda</a></li>
+                        <li><a href='/' onClick={() => setIsMenuOpen(false)}>Produk</a></li>
+                        <li><a href='/' onClick={() => setIsMenuOpen(false)}>Blog</a></li>
+                        <li><a href='/' onClick={() => setIsMenuOpen(false)}>Kontak</a></li>
+                    </ul>
+                </div>
+
+                <div className='navbar-socials'>
+                    <a href='/' aria-label='Whatsapp'><i className='fa-brands fa-whatsapp'></i></a>
+                    <a href='/' aria-label='Instagram'><i className='fa-brands fa-instagram'></i></a>
+                    <a href='/' aria-label='Facebook'><i className='fa-brands fa-facebook-f'></i></a>
+                </div>
+
+                <div className='navbar-lang'>
+                    <div className='lang-wrapper' onClick={() => setIsLangOpen(!isLangOpen)}>
+                        <i className='fa-solid fa-globe'></i>
+                        <span className='lang-selected-text'>{language}</span>
+                        <i className={`fa-solid fa-chevron-down navbar-chevron ${isLangOpen ? 'rotate' : ''}`}></i>
+
+                        {isLangOpen && (
+                            <ul className='lang-list'>
+                                <li onClick={() => handleLanguageChange('ID')}>ID</li>
+                                <li onClick={() => handleLanguageChange('EN')}>EN</li>
+                            </ul>
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
-    );
+    )
 }
 
 export default Navbar;
